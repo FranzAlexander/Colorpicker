08cc80ecd2d6005e9391d9a142ab4553291621e4
FranzAlexander <alexbalcombe4@gmail.com>
2017-11-27 13:08:39 +0800

    Uploading colorpicker

---
+++ ColorPicker/ChoosenColors.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+namespace ColorPicker
+{
+    public class ChoosenColors
+    {
+        private Color color;
+
+       public string colorName { get; set; }
+       public string colorHex { get; set; }
+       public string colorRGBA { get; set; }
+       public string colorHue { get; set; }
+
+        /// <summary>
+        /// Setting the color values.
+        /// </summary>
+        /// <param name="color"></param>
+        public ChoosenColors(Color color)
+        {
+            this.color = color;
+            this.colorName = color.Name;
+            this.colorHex = color.ToArgb().ToString("X6");
+            this.colorRGBA = color.R.ToString() + ",";
+            this.colorRGBA += color.G.ToString() + ",";
+            this.colorRGBA += color.B.ToString() + ",";
+            this.colorRGBA += color.A.ToString();
+            this.colorHue = color.GetHue().ToString();
+
+            //Checkng if the the first character of colorName is upper case because if it is not then it is the hex value not the name.
+            if (!char.IsUpper(colorName[0]))
+            {
+                colorName = "Color Name Not Stored In DataBase";
+            }
+        }
+    }
+}
---
+++ ColorPicker/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace ColorPicker
+{
+    public partial class Form1 : Form
+    {
+        //Binding list to display the colors in the data grid view.
+        BindingList<ChoosenColors> colorSelected = new BindingList<ChoosenColors>();
+
+        //Color object
+        Color currentColor;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Button event to handle the showing of the color dialog as well as sending the chosen color to the color object.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            pictureBox1.BackColor = colorDialog1.Color;
+
+            currentColor = colorDialog1.Color;
+        }
+
+        /// <summary>
+        /// Adds the current color to the data binding list so that it can show it in the data grid view.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            colorSelected.Add(new ChoosenColors(currentColor));
+
+            dataGridView1.DataSource = colorSelected;
+        }
+
+        /// <summary>
+        /// Serializes object to json then prints it the text file in json format.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string json = JsonConvert.SerializeObject(colorSelected, Formatting.Indented);
+
+            File.WriteAllText(@"E:\ColorPicker\ColorPicker\ColorsChosen.txt", json); //If file is not there it will create it.
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control)
+            {
+                switch (e.KeyCode)
+                {
+                    case Keys.Q:
+                        Close();
+                        break;
+                }
+            }
+
+        }
+
+      
+    }
+}
